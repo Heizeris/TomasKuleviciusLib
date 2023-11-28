3ebaac06824f15a7e5c831c069ac1079e755bc15
Heizeris <ktomask464@gmail.com>
2023-11-28 13:45:09 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+import lt.techin.library.BookCatalog;
+import lt.techin.library.test.AbstractLibraryTest;
+import org.junit.jupiter.api.Test;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
