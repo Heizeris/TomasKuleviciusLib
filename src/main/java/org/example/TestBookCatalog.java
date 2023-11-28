4bdd673cbea567f284bc5e06dfbeb7d9f281e9f1
Heizeris <ktomask464@gmail.com>
2023-11-28 14:53:03 +0200

    "Created Catalogimpl"

--- src/main/java/org/example/TestBookCatalog.java
+++ src/main/java/org/example/TestBookCatalog.java
@@ -3,6 +3,7 @@ package org.example;
 import lt.techin.library.Book;
 import lt.techin.library.BookCatalog;
 import lt.techin.library.test.AbstractLibraryTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestBookCatalog extends AbstractLibraryTest {
@@ -11,6 +12,7 @@ public class TestBookCatalog extends AbstractLibraryTest {
         return new CatalogImpl();
         
     }
+
 
 
 }
