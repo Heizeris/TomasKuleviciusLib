5aa8158c12e7b15786661a3826d6419674a0e023
Heizeris <ktomask464@gmail.com>
2023-11-28 14:43:31 +0200

    "Created TestBookCatalog"

---
+++ src/main/java/org/example/TestBookCatalog.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import lt.techin.library.Book;
+import lt.techin.library.BookCatalog;
+import lt.techin.library.test.AbstractLibraryTest;
+import org.junit.jupiter.api.Test;
+
+public class TestBookCatalog extends AbstractLibraryTest {
+    @Override
+    protected BookCatalog createBookCatalog() {
+        return new CatalogImpl();
+        
+    }
+
+
+}
